5d24091d2a3bb74ce1fd6f8a9d4c6d9685cad8b8
Sagara Jayathilaka Alahakoon <sgrsagara@gmail.com>
2015-11-04 20:50:35 +0530

    Incomplete EIP file handling
    Incomplete EIP file handling

---
+++ AMEX/Incomplete_EIP_handle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+###
+##CIP-233 - Handling possible incomplete EIP file pickup
+###
+##1. Retrieve from EIP APA (SFTP) | $CORE_DATA/user/btareconciliation/sftp/eip/apa/upload/ready
+cd $CORE_DATA/user/btareconciliation/sftp/eip/apa/upload/
+APA = $(find . -maxdepth 1 -mmin +15 -type f)
+mv $APA $CORE_DATA/user/btareconciliation/sftp/eip/apa/upload/ready/
+##
+##2. Retrieve from EIP EMEA (SFTP) | $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/ready
+cd $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload
+APA = $(find . -maxdepth 1 -mmin +15 -type f)
+mv $APA $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/ready
+##
+##3. Retrieve from EIP LAC (SFTP) | $CORE_DATA/user/btareconciliation/sftp/eip/lac/upload/ready
