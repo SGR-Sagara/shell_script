66545eaf4f5c21c761ccad111cb6a8a24c69d060
Sagara Jayathilaka Alahakoon <sgrsagara@gmail.com>
2015-11-04 21:56:07 +0530

    corrected

--- AMEX/Incomplete_EIP_handle.sh
+++ AMEX/Incomplete_EIP_handle.sh
@@ -4,16 +4,16 @@
 ###
 ##1. Retrieve from EIP APA (SFTP) | $CORE_DATA/user/btareconciliation/sftp/eip/apa/upload/ready
 cd $CORE_DATA/user/btareconciliation/sftp/eip/apa/upload/
-APA = $(find . -maxdepth 1 -mmin +15 -type f)
+APA=$(find . -maxdepth 1 -mmin +15 -type f)
 mv $APA $CORE_DATA/user/btareconciliation/sftp/eip/apa/upload/ready/
 ##
 ##2. Retrieve from EIP EMEA (SFTP) | $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/ready
 cd $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/
-APA = $(find . -maxdepth 1 -mmin +15 -type f)
-mv $APA $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/ready
+EMEA=$(find . -maxdepth 1 -mmin +15 -type f)
+mv $EMEA $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/ready
 ##
 ##3. Retrieve from EIP LAC (SFTP) | $CORE_DATA/user/btareconciliation/sftp/eip/lac/upload/ready
 cd $CORE_DATA/user/btareconciliation/sftp/eip/lac/upload/
-APA = $(find . -maxdepth 1 -mmin +15 -type f)
-mv $APA $CORE_DATA/user/btareconciliation/sftp/eip/lac/upload/ready
+LAC=$(find . -maxdepth 1 -mmin +15 -type f)
+mv $LAC $CORE_DATA/user/btareconciliation/sftp/eip/lac/upload/ready
 ##
