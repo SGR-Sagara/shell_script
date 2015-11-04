b6dbf88097f56b87bfe589e4f3ba094f54c28ca1
Sagara Jayathilaka Alahakoon <sgrsagara@gmail.com>
2015-11-04 20:53:11 +0530

    New location added
    Incomplete EIP file handling

--- AMEX/Incomplete_EIP_handle.sh
+++ AMEX/Incomplete_EIP_handle.sh
@@ -8,8 +8,12 @@ APA = $(find . -maxdepth 1 -mmin +15 -type f)
 mv $APA $CORE_DATA/user/btareconciliation/sftp/eip/apa/upload/ready/
 ##
 ##2. Retrieve from EIP EMEA (SFTP) | $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/ready
-cd $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload
+cd $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/
 APA = $(find . -maxdepth 1 -mmin +15 -type f)
 mv $APA $CORE_DATA/user/btareconciliation/sftp/eip/emea/upload/ready
 ##
 ##3. Retrieve from EIP LAC (SFTP) | $CORE_DATA/user/btareconciliation/sftp/eip/lac/upload/ready
+cd $CORE_DATA/user/btareconciliation/sftp/eip/lac/upload/
+APA = $(find . -maxdepth 1 -mmin +15 -type f)
+mv $APA $CORE_DATA/user/btareconciliation/sftp/eip/lac/upload/ready
+##
